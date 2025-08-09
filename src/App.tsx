adec76c79e83d38ee3b13f78d434cad7975ee30e
Dulanja <dula.tr14@gmail.com>
2025-08-09 15:06:49 +0530

    Fix homepage URL and routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/footballlablk.github.io">
       <div className="App">
         <AnimatePresence mode="wait">
           {isLoading ? (
